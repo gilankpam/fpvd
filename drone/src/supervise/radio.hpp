73f74f4b403ded0cbaddef6f374a0d409f55944d
Gilang <gilankpam@gmail.com>
2026-06-12 00:42:53 +0700

    drone/radio: fix stale FPVD_TXPOWER comment -> FPVD_TXPOWER_DBM

--- drone/src/supervise/radio.hpp
+++ drone/src/supervise/radio.hpp
@@ -15,7 +15,7 @@ struct RadioResult {
 };
 
 // Execute `scriptPath`, passing the relevant link.* fields as env vars
-// (FPVD_CHANNEL, FPVD_WIDTH, FPVD_TXPOWER, FPVD_MTU). Capture stdout
+// (FPVD_CHANNEL, FPVD_WIDTH, FPVD_TXPOWER_DBM, FPVD_MTU). Capture stdout
 // (expected key=value lines) and stderr. Returns within ~30 seconds.
 RadioResult bringUpRadio(const std::string& scriptPath, const Config& c);
 
